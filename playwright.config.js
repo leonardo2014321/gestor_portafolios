98edd3f3b7e0207a90d4ef8456558d576179fe94
serpientinon <61918287+serpientinon@users.noreply.github.com>
2026-04-28 00:20:13 -0400

    chore: update Playwright configuration for enhanced reporting and test resilience, and add CA root certificate bundle.

--- playwright.config.js
+++ playwright.config.js
@@ -19,13 +19,20 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { open: 'never', outputFolder: 'playwright-report' }],
+    ['list']
+  ],
+
+  outputDir: 'test-results',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
